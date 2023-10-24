9958a624c537dad8e1c5c41815af14edae508e21
Monali Valunj <monavalunj96@gmail.com>
2023-10-23 20:52:13 -0700

    completed the script

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let form;
+let empId;
+let empName; 
+let email;
+let ext;
+let dept;
+let btnSubmit; 
+const $ = (id) => document.getElementById(id);
+form = $('empForm');
+empId = $('id');
+empName = $('name');
+email = $('email');
+ext = $('ext');
+dept = $('dept');
+btnSubmit = $('btn-submit');
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+
+    console.log("Id: "+ empId.value);
+    console.log("Name: "+ empName.value);
+    console.log("email: "+ email.value);
+    console.log("Extension: "+ ext.value);
+    console.log("Department: "+ dept.value);
+
+});
